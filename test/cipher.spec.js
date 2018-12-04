2fa8a1ee9913ac6de8219abf793ce48d1fe3109d
GloriaMoraga <silmeriel@gail.com>
2018-12-04 08:57:52 -0300

    añadiendo flex box css

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -3,16 +3,17 @@ describe('cipher', () => {
   it('debería ser un objeto', () => {
     assert.equal(typeof cipher, 'object');
   });
-
+});
   describe('cipher.encode', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
-    assert.equal(cipher.encode, '');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+    assert.equal(cipher.encode ('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 33), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
   });
+});
 
   describe('cipher.decode', () => {
 
@@ -20,7 +21,9 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33',() => {
+    assert.equal(cipher.encode ('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 33), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
   });
-
+  
+ 
 });
