e003280b140ea0c8837bc86943feb5e655fb264b
GloriaMoraga <silmeriel@gail.com>
2018-12-11 09:09:05 -0300

    actualizando test

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -13,12 +13,16 @@ describe('cipher', () => {
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
     assert.equal(cipher.encode ('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 33), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
   });
-  it('debería retornar "abc" para "DEF" con offset 3',() => {
-    assert.equal(cipher.encode ('abc', 3), 'DEF');
+  it('debería retornar "abc" para "def" con offset 3',() => {
+    assert.equal(cipher.encode ('abc', 3), 'def');
   });
 
   it('debería retornar " " para espacio vacio con offset 3',() => {
     assert.equal(cipher.encode (' ', 3), ' ');
+  });
+
+  it('debería retornar 2 para un numero con offset 3',() => {
+    assert.equal(cipher.encode (' 2 ', 3), ' 2 ');
   });
   
  
@@ -33,13 +37,16 @@ describe('cipher', () => {
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33',() => {
     assert.equal(cipher.decode ('HIJKLMNOPQRSTUVWXYZABCDEFG', 33), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
   });
-  it('debería retornar "abc" para "XYZ" con offset 3',() => {
-    assert.equal(cipher.decode ('abc', 3), 'XYZ');
+  it('debería retornar "ABC" para "XYZ" con offset 3',() => {
+    assert.equal(cipher.decode ('ABC', 3), 'XYZ');
   });
 
   it('debería retornar " " para espacio vacio con offset 3',() => {
     assert.equal(cipher.decode (' ', 3), ' ');
   });
-  
  
+  
+  it('debería retornar "abc" para "xyz" con offset 3',() => {
+    assert.equal(cipher.decode ('abc', 3), 'xyz');
+  });
 });
